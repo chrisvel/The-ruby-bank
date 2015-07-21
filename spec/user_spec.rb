4605f5dce61d22062784042829cc7861a837d9ad
Chris Veleris <chris.veleris@me.com>
2015-07-21 16:24:58 +0300

    Added bcrypt gem

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,10 +1,13 @@
+require 'rails_helper'
 require 'user'
 
 describe User do
+
   it "is valid with a firstname, lastname and email" do
-  it "is invalid without a firstname" do
-  it "is invalid without a lastname" do
-  it "is invalid without an email address" do
-  it "is invalid with a duplicate email address" do
-  it "returns a contact's full name as a string" do
+    user = User.new(
+      name: 'John Murdock',
+      email: 'someone@somewhere.com')
+      expect(contact).to be_valid
+  end
+
 end
