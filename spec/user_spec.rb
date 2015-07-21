e59d032bc19cda3ef45330a6508c4e66464dd5a4
Chris Veleris <chris.veleris@me.com>
2015-07-21 16:35:17 +0300

    Validates presence of user details

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,11 +3,58 @@ require 'user'
 
 describe User do
 
-  it "is valid with a firstname, lastname and email" do
+  it "is valid with a name, email and password" do
     user = User.new(
       name: 'John Murdock',
-      email: 'someone@somewhere.com')
-      expect(contact).to be_valid
+      email: 'someone@somewhere.com',
+      password: 'password',
+      password_confirmation: 'password')
+      expect(user).to be_valid
+  end
+
+  it "is valid without a name" do
+    user = User.new(
+      name: '',
+      email: 'someone@somewhere.com',
+      password: 'password',
+      password_confirmation: 'password')
+      expect(user).to be_valid
+  end
+
+  it "is invalid without an email" do
+    user = User.new(
+      name: 'John Murdock',
+      email: '',
+      password: 'password',
+      password_confirmation: 'password')
+      expect(user).not_to be_valid
+  end
+
+  it "is invalid without a password" do
+    user = User.new(
+      name: 'John Murdock',
+      email: 'someone@somewhere.com',
+      password: '',
+      password_confirmation: 'password')
+      expect(user).not_to be_valid
+  end
+
+  it "is invalid without a password_confirmation" do
+    user = User.new(
+      name: 'John Murdock',
+      email: 'someone@somewhere.com',
+      password: 'password',
+      password_confirmation: '')
+      expect(user).not_to be_valid
+  end
+
+  it "is invalid without a password and a password_confirmation" do
+    user = User.new(
+      name: 'John Murdock',
+      email: 'someone@somewhere.com',
+      password: '',
+      password_confirmation: 'password')
+      expect(user).not_to be_valid
   end
 
 end
